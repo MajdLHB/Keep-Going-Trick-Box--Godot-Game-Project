7890492fb58f96cf063178441557f9bf342b7aac
Majd Lahbib <109460986+MajdLHB@users.noreply.github.com>
2024-08-22 15:29:34 +0100

    Making the resolution vhange option making

--- Godot Project/Scripts/Options/Resolution.gd
+++ Godot Project/Scripts/Options/Resolution.gd
@@ -1,11 +1,32 @@
-extends Node
+extends OptionButton
 #Resolution
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if Variables.Resolution == "800x600":
+		Variables.selected_resolution_ID = 0
+	if Variables.Resolution == "1280x720":
+		Variables.selected_resolution_ID = 1
+	if Variables.Resolution == "1920x1080":
+		Variables.selected_resolution_ID = 2
+	if Variables.Resolution == "2560x1440":
+		Variables.selected_resolution_ID = 3
+	if Variables.Resolution == "3840x2160":
+		Variables.selected_resolution_ID = 4
+		
+	self.connect("item_selected", Callable(self, "on_selected"))
+	self.add_item("800x600")
+	self.add_item("1280x720")
+	self.add_item("1920x1080")
+	self.add_item("2560x1440")
+	self.add_item("3840x2160")
+	
+	Variables._save_settings()
+	
+	self.select(Variables.selected_resolution_ID)
 
+func on_selected(nothing):
+	Variables.selected_resolution_ID = self.get_selected_id()
+	Variables.Resolution = self.get_item_text(self.get_selected_id())
+	Variables._save_settings()
+	Variables.Set_Settings()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
--- Godot Project/Scripts/Variables.gd
+++ Godot Project/Scripts/Variables.gd
@@ -2,12 +2,22 @@ extends Node
 
 var Activate_button = true
 var VSYNC = false
-var Resolution = "1920x1080"
+var Resolution = "1280x720"
+var selected_resolution_ID = 0
 var FullScreen = false
+var settings_file_path = "res://Settings file/Settinggs.cfg"
+var config = ConfigFile.new()
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	config.load(settings_file_path)
+	_load_settings()
 	Set_Settings()
+	_save_settings()
+
+
 
 
 func Set_Settings():
@@ -25,3 +35,28 @@ func Set_Settings():
 	ProjectSettings.set("display/mouse_cursor/custom_image", "res://Images/Mouse cursor/Cursor.jpg")
 	DisplayServer.window_set_size(Vector2i(FirstResolution.to_int(), SecondResolution.to_int()))
 
+func _save_settings():
+	config.set_value("display", "vsync", VSYNC)
+	config.set_value("display", "resolution", Resolution)
+	config.set_value("display", "red_Id", selected_resolution_ID)
+	config.set_value("display", "fullscreen", FullScreen)
+	config.save(settings_file_path)
+	
+func Load_Vsync():
+	VSYNC = config.get_value("display", "vsync")
+
+func Load_Resolution():
+	Resolution = config.get_value("display", "resolution")
+
+func Load_res_ID():
+	selected_resolution_ID = config.get_value("display", "red_Id")
+	print(selected_resolution_ID)
+
+func Load_FullScreen():
+	FullScreen = config.get_value("display", "fullscreen")
+
+func _load_settings():
+	Load_Vsync()
+	Load_Resolution()
+	Load_res_ID()
+	Load_FullScreen()
